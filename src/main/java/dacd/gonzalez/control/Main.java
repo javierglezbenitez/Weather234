449684a5bbda981c290b3a5d859cdadd6b0dc8fe
cgsos <javierglezbenitez@gmail.com>
2023-11-09 16:44:04 +0000

    Method GetWeather

--- src/main/java/dacd/gonzalez/control/Main.java
+++ src/main/java/dacd/gonzalez/control/Main.java
@@ -15,50 +15,5 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            String url = "https://api.openweathermap.org/data/2.5/forecast?lat=50.5456&lon=-15.41915&appid=51cb3b621914382d96a450c0f3451582";
-            String jsonString = Jsoup.connect(url).ignoreContentType(true).execute().body();
-
-            Gson gson = new Gson();
-            JsonObject weathers = gson.fromJson(jsonString, JsonObject.class);
-            JsonArray lists = weathers.getAsJsonObject().getAsJsonArray("list");
-
-            ArrayList<Weather> weatherList = new ArrayList<>();
-
-            for (JsonElement list: lists) {
-                JsonObject weather = list.getAsJsonObject();
-                JsonObject main = weather.get("main").getAsJsonObject();
-                JsonObject clouds = weather.get("clouds").getAsJsonObject();
-                JsonObject wind = weather.get("wind").getAsJsonObject();
-
-                double temp = main.get("temp").getAsDouble();
-                int humidity = main.get("humidity").getAsInt();
-                int all = clouds.get("all").getAsInt();
-                double speed = wind.get("speed").getAsDouble();
-                Double pop = weather.get("pop").getAsDouble();
-                int dt = weather.get("dt").getAsInt();
-
-                long unixTimestamp = dt;
-                Instant weatherInstant = Instant.ofEpochSecond(unixTimestamp);
-
-                Weather weatherObject = new Weather(temp, humidity, all, speed, pop, weatherInstant);
-                weatherList.add(weatherObject);
-
-
-
-            }
-
-            for (Weather weatherIter : weatherList) {
-                System.out.println("Temp:"+weatherIter.getTemp());
-                System.out.println("clouds:"+weatherIter.getAll());
-                System.out.println("wind:"+weatherIter.getSpeed());
-                System.out.println("humidity:"+weatherIter.getHumidity());
-                System.out.println("pop:"+weatherIter.getPop());
-                System.out.println("dt:"+weatherIter.getDt()+"\n");
-            }
-
-        }catch (Exception e){
-            throw new RuntimeException();
-        }
     }
 }
---
+++ src/main/java/dacd/gonzalez/control/WeatherController.java
@@ -0,0 +1,35 @@
+package dacd.gonzalez.control;
+
+import dacd.gonzalez.model.Location;
+
+import java.time.Instant;
+import java.util.ArrayList;
+
+public class WeatherController {
+    public static void main(String[] args) {
+        Location lanzarote = new Location("Lanzarote", 28.96302, -13.54769);
+        Location fuerteventura = new Location("Fuerteventura", 28.50038, -13.86272);
+        Location GranCanaria = new Location("Gran Canaria", 28.09973, -15.41343);
+        Location tenerife = new Location("Tenerife", 28.46824, -16.25462);
+        Location hierro = new Location("El hierro", 27.80628, -17.91578);
+        Location gomera = new Location("La Gomera", 28.09163, -17.11331);
+        Location palma = new Location("La Palma", 28.68351, -17.76421);
+        Location graciosa = new Location("La Graciosa", 29.23147, -13.50341);
+
+        ArrayList<Location> islands = new ArrayList<>();
+        islands.add(lanzarote);
+        islands.add(fuerteventura);
+        islands.add(GranCanaria);
+        islands.add(tenerife);
+        islands.add(hierro);
+        islands.add(gomera);
+        islands.add(palma);
+        islands.add(graciosa);
+
+
+        for (int i = 0; i < 8; i++) {
+            WeatherProvider.WeatherGet(islands.get(i));
+
+        }
+    }
+}
